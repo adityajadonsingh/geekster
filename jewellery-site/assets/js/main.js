352d403eda5952882ad831bb7746ca38abd83fd8
Aditya Jadon <51057603+adityajadonsingh@users.noreply.github.com>
2025-01-29 22:06:22 +0530

    Project Completed

--- jewellery-site/assets/js/main.js
+++ jewellery-site/assets/js/main.js
@@ -1,19 +1,64 @@
-var swiper = new Swiper(".products", {
-    loop: true,
-    cssMode: true,
-    autoplay: {
-        delay: 2500,
-        pauseOnMouseEnter: true,
+//Header Js
+
+const header = document.querySelector("header");
+const menuBtn = document.querySelector(".mobile-menu i");
+const menuBar = document.querySelector("header .btm-nav");
+
+menuBtn.addEventListener("click", ()=> menuBar.classList.contains("open") ? menuBar.classList.remove("open") : menuBar.classList.add("open"));
+
+window.addEventListener("scroll", () => {
+
+  if (window.scrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
+
+//Slider Js
+
+var productSlider = new Swiper(".products", {
+  loop: true,
+  cssMode: true,
+  autoplay: {
+    delay: 2500,
+    pauseOnMouseEnter: true,
+  },
+  speed: 500,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    380: {
+      slidesPerView: 2,
     },
-    speed: 500,
-    slidesPerView: 4,
-    spaceBetween: 20,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+    768: {
+      slidesPerView: 3,
     },
-  });
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var testimonials = new Swiper(".testimonials", {
+  loop: true,
+  cssMode: true,
+  autoplay: {
+    delay: 3500,
+    pauseOnMouseEnter: true,
+  },
+  speed: 2000,
+  slidesPerView: 1,
+  pagination: {
+    el: ".rigth-side .swiper-pagination",
+    clickable: true,
+  },
+});
